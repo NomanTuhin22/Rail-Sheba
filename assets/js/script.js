faf55b0c0b5619a503ac7faf2684f8ce7e3cadfc
Noman <tuhin002020@gmail.com>
2024-08-08 09:58:21 +0600

    some chnage and rename file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+
 // *********Card 1 *****selectior******
 let card_One = document.getElementById('card_One');
 // *********Card 2 ****selectior*******
@@ -10,3 +11,8 @@ let card_Two = document.getElementById('card_Two');
 let remember = document.getElementById('remember');
 let label_Password = document.getElementById('label_Password');
 let forget_Password = document.getElementById('forget_Password');
+// *********Card 3 *****selectior******
+let card_Three = document.getElementById('card_Three');
+let get_Email = document.getElementById('get_Email');
+
+console.log(email.value);
